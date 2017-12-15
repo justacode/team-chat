a9e0a504ae5eff2912eb017a87c51b2dfda9715c
Justacode <mustafinkzn97@gmail.com>
2017-12-15 20:43:53 +0300

    Changed redis configuration

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,3 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
+encoded_url = URI.encode(ENV["REDISTOGO_URL"])
+uri = URI.parse(encoded_url)
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
