4a12dd293758f54b8ac35b392e79b5630166352e
Justacode <mustafinkzn97@gmail.com>
2017-12-16 09:40:48 +0300

    Changed redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
 if Rails.env == 'production'
   uri = URI.parse(ENV["REDISTOGO_URL"])
-  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
