b6068bb022e2c38cd47473fc8676b3f879499e5b
Marsel Mustafin <mustafinkzn97@gmail.com>
2018-02-11 00:49:47 +0300

    Fixed online status bug

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -5,6 +5,11 @@ class UserDecorator < Draper::Decorator
     "#{object.first_name} #{object.last_name}"
   end
 
+  def is_online?
+    puts object.list.include? full_name
+    object.list.include? full_name
+  end
+
   def registrated_at
     object.created_at.strftime('%m/%d/%y')
   end
